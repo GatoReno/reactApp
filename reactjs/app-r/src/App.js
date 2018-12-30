b13115f9809e72ee183ba90541455adda33f91d5
DMN <dmn@DMNs-MacBook-Pro.local>
2018-12-29 21:20:54 -0600

    no se si commitie algo o no aquí

--- reactjs/app-r/src/App.js
+++ reactjs/app-r/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
+
+
         <header className="App-header">
+          
+         
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload yeah :3 ...
           </p>
           <a
             className="App-link"
@@ -21,6 +27,7 @@ class App extends Component {
           </a>
         </header>
       </div>
+    
     );
   }
 }
